39a6c05a79b6f0e81f4499b454c8443c21febb67
Codby <87335804+TheCodby@users.noreply.github.com>
2023-08-18 00:53:32 +0300

    initial commit

---
+++ app1/index.js
@@ -0,0 +1,15 @@
+"use strict";
+import express from "express";
+// Constants
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.get("/", (req, res) => {
+  res.send(process.env.APP_NAME);
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
